3482c37a2a5a48b3ca6abd928dae30575ac0c57c
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2022-02-05 12:54:20 +1100

    Pseudo code to like target followers images

--- instagram/likeFollowersProcess.js
+++ instagram/likeFollowersProcess.js
@@ -1,3 +1,4 @@
+const instagram = require('.');
 const { delay } = require('.');
 
 const navigateToFollowerPage = async (follower) => {
@@ -11,10 +12,42 @@ const navigateToFollowerPage = async (follower) => {
 	});
 };
 
+const openFollowersModal = async (follower) => {
+	await instagram.page.click(`a[href="/${follower}/followers/"]`);
+	await instagram.page.waitForNavigation({
+		waitUntil: 'networkidle2',
+	});
+	await delay(2000);
+};
+
+const iterateThroughfollowers = () => {
+	const followers = await instagram.page.$$(
+		'div[aria-label="Followers"] li a[title]'
+	);
+
+	for (let i=0; i < followers.length; i++) {
+		const follower = followers[i];
+		await instagram.page.click(follower, { button: 'middle' });
+		// if not private page
+			//	get number of images
+			//	make array of six random numbers
+			//	for each number in array
+				//	click on that image
+				//	if like button
+					//	click like button
+				//	else
+					//	add another number to the array (that isn't already there)
+				//	close image modal
+		// close window regardless
+	}
+};
+
 module.exports = async (followers = []) => {
 	await delay(2000);
 
-	for (let follower of followers) {
-		navigateToFollowerPage(follower);
+	for (let followersTarget of followersTargets) {
+		await navigateToFollowerPage(followersTarget);
+		await openFollowersModal(followersTarget);
+		await iterateThroughfollowers();
 	}
 };
